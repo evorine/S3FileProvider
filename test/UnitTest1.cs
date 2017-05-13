5c55dc92c190fd6d39fecb5db50a0c89bbadc92a
Yilmaz Dogus <yilmazdogus@hotmail.com>
2017-05-13 18:29:56 +0300

    Added test project

---
+++ test/UnitTest1.cs
@@ -0,0 +1,13 @@
+using System;
+using Xunit;
+
+namespace Evorine.Engine.FileProviders.S3.Test
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+        }
+    }
+}
